3696e2f0a3816650346668cbdb321e6c3d9ff522
Jean-François Rey <jean-francois.rey@inrae.fr>
2021-01-21 21:33:52 +0100

    fix bug unattend

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 
 OS="windows"
 VERSION="10"
-HEADLESS=true
-ISO="19041.264.200511-0456.vb_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_fr-fr.iso"
-CHECKSUM="2965FD214FE77D3C80BADCF0B907399DA6636675422C64AE3C0D5DABB2B90C66"
+HEADLESS=false
+ISO="19042.631.201119-0144.20h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
+CHECKSUM="32C7B0A51A48CC4F67C250BE4FE2B384FEBB9CC864C5B77A052D4E2845394EAC"
 
 VM_NAME="${OS}${VERSION}"
 
@@ -16,7 +16,7 @@ packer build \
   -var "headless=${HEADLESS}" \
 	-var "iso_url=${ISO}" \
 	-var "iso_checksum=${CHECKSUM}" \
-	-var "autounattend=./answer_file/autounattend.xml" \
+	-var "autounattend=./answer_file/win10Enterprise/autounattend.xml" \
   -var-file="windows10ENTERPRISE.json" \
   main_template.json 
 
