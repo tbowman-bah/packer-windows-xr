2be5cc38d0c99e0870986ed848f7c5e9a85d9620
Jean-François Rey <jf.rey.public@gmail.com>
2021-01-25 21:43:07 +0100

    msys2 install pkg bugfix

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -eux
 
-
 OS="Windows10_64_EntEval"
 VERSION="4.0.3"
 R_VERSION="4.0.3"
-HEADLESS=false
+HEADLESS=true
 ISO="19042.631.201119-0144.20h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
 CHECKSUM="32C7B0A51A48CC4F67C250BE4FE2B384FEBB9CC864C5B77A052D4E2845394EAC"
 AUTOUNATTEND="./answer_file/win10Enterprise/autounattend.xml"
@@ -12,6 +11,16 @@ BOX_TAG="VMR/${OS}-R"
 DESCRIPTION="${OS} + R ${R_VERSION} + Rtools"
 
 VM_NAME="${OS}-R${R_VERSION}"
+
+usage() { echo "Usage: $0 [-h] [-c <vagrantcloudtoken>]" 1>&2; exit 1; }
+
+while getopts c:h flag
+do
+    case "${flag}" in
+        c) VAGRANTCLOUD_TOKEN=${OPTARG};;
+        h) usage();;
+    esac
+done
 
 packer build \
   -only=virtualbox-iso \
@@ -22,7 +31,7 @@ packer build \
 	-var "iso_checksum=${CHECKSUM}" \
 	-var "autounattend=${AUTOUNATTEND}" \
   -var "tagbox=${BOX_TAG}" \
-  -var "vagrantcloud_token=" \
+  -var "vagrantcloud_token=${VAGRANTCLOUD_TOKEN}" \
   -var "version=${VERSION}" \
   -var "version_description=${DESCRIPTION}" \
   -var-file="windows10ENTERPRISE.json" \
